8238f88610f5ee4fa4d1868f8954fe13cbef8b9f
Duonghoangbn2001 <hoanglinhbn2001@gmail.com>
2023-07-13 18:59:48 +0700

    first commit

---
+++ kenh/kenh1.php
@@ -0,0 +1,4 @@
+#EXTM3U
+#-------------------VTV-------------------#
+#EXTINF:-1 group-title="VTV" tvg-logo="https://i.imgur.com/6047gMa.jpg",VTV1 (HD 8Mbps)
+https://live-zlr1.tv360.vn/manifest/vtv2/playlist_1080p.m3u8
